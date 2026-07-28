77c8771148d54396e260a43e1ea5f76f8958dc5e
Yash tambade <yashtambade56@gmail.com>
2026-07-28 21:09:29 +0530

    Add files via upload

---
+++ Day 01/add.java
@@ -0,0 +1,9 @@
+public class add {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 12;
+        int sum = a + b;
+
+        System.out.println("The sum of " + a + " and " + b + " is " + sum);
+    }
+}

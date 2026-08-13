ce5dffa12bf52789393c4b098e9248b0b636ba1c
Yash tambade <yashtambade56@gmail.com>
2026-08-13 15:11:52 +0530

    Add student menu with options for profile and attendance

---
+++ Day 05/studentMenu.java
@@ -0,0 +1,18 @@
+public class studentMenu {
+    
+    public static void main(String[] args) {
+        
+        int choice = 0;
+
+        do{
+
+            System.out.println("1. View Profile");
+            System.out.println("2. View Attendance");
+            System.out.println("3. Exit");
+
+            choice = 3;
+        } while(choice != 3);
+
+        System.out.println("exiting the menu");
+    }
+}
